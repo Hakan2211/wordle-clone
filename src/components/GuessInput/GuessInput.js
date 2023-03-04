b8590c97365972fe7224846607701b289f27caf0
Hakan Bilgic <hbilgic1992@gmail.com>
2023-03-04 14:37:25 +0100

    Exercise 1: GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({ guess });
+    setGuess("");
+  };
+  return (
+    <form className="guess-input-wrapper" onSubmit={onSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        pattern="^[A-Z]{5}$"
+        title="5 letter word"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
